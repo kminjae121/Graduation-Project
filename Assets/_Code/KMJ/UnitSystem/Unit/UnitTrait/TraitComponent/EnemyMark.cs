ebb5f783a8f95e20803aca79a64411dae0e6d683
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-05-19 23:52:19 +0900

    feat : SkillCompo

---
+++ Assets/_Code/KMJ/UnitSystem/Unit/UnitTrait/TraitComponent/EnemyMark.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UnitSystem.TraitSystem
+{
+    public class EnemyMark : MonoBehaviour
+    {
+        [SerializeField] private GameObject targetMark;
+
+        [SerializeField] private int _maxCnt;
+        private int _markCnt = 0;
+
+        private bool _isMarked = false;
+        
+        public void SetMark()
+        {
+            if (_markCnt >= _maxCnt)
+                _isMarked = true;
+
+            if (_isMarked)
+                return;
+            
+            if (_markCnt == 0)
+                targetMark.SetActive(true);
+            
+            _markCnt += 1;
+        }
+    }
+}
